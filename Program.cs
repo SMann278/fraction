61671c5c607ffb19c2784f1e81eed04e8a376a7f
SMann278 <69685417+SMann278@users.noreply.github.com>
2020-10-02 10:01:45 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,313 @@
+//Simcha Mann
+
+using System;
+
+namespace ConsoleApp2
+{
+    public class Fraction
+    {
+        readonly int num;
+        readonly int denom;
+
+        public Fraction()
+        {
+            this.num = 0;
+            this.denom = 1;
+        }
+
+        public Fraction(int num, int denom)
+        {
+            this.num = num;
+            if (denom != 0)
+            {
+                this.denom = denom;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Denominator can't be zero");
+            }
+
+            Reduce(this);
+        }
+
+        public Fraction(int num)
+        {
+            this.num = num;
+            this.denom = 1;
+        }
+
+        public Fraction(Fraction f1)
+        {
+            this.num = f1.num;
+            if (f1.denom != 0)
+            {
+                this.denom = f1.denom;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("Denominator can't be zero");
+            }
+
+            Reduce(this);
+        }
+
+        public int Numerator
+        {
+            get { return num; }
+        }
+
+        public int Denominator
+        {
+            get { return denom; }
+        }
+
+        public static Fraction Reduce(Fraction f1)
+        {
+            int factor = f1.Gcf(f1);
+            return new Fraction(f1.num / factor, f1.denom / factor);
+        }
+
+        public int Gcf(Fraction f1)
+        {
+            int i;
+            if (f1.num <= f1.denom)
+            {
+                i = Math.Abs(f1.num);
+            }
+            else
+            {
+                i = Math.Abs(f1.denom);
+            }
+
+            while (i > 0)
+            {
+                if (f1.num % i == 0 && f1.denom % i == 0)
+                {
+                    return i;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            return i;
+        }
+
+        public static int Lcm(Fraction f1, Fraction f2)
+        {
+            int lcm;
+            if (f1.Denominator >= f2.Denominator)
+            {
+                lcm = f1.Denominator;
+            }
+            else
+            {
+                lcm = f2.Denominator;
+            }
+            while (lcm%f1.Denominator!=0 || lcm%f2.Denominator!=0 )
+            {
+                lcm++;
+            }
+            return lcm;
+        }
+
+        public static Fraction operator +(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd/f1.denom);
+            int f2Num = f2.num * (lcd/f2.denom);
+            //create new fraction using sum and common denom
+            //in the constructor it will be automatically reduced
+            Fraction answer = new Fraction(f1Num + f2Num, lcd);
+            return answer;
+        }
+
+        public static Fraction operator -(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            //create new fraction using difference and common denom
+            //in the constructor it will be automatically reduced
+            Fraction answer = new Fraction(f1Num - f2Num, lcd);
+            return answer;
+        }
+
+        public static Fraction operator *(Fraction f1, Fraction f2)
+        {
+            //create new fraction using mult of num and denom
+            //in the constructor it will be automatically reduced
+            Fraction answer = new Fraction(f1.num * f2.num, f1.denom * f2.denom);
+            return answer;
+        }
+
+        public static Fraction operator /(Fraction f1, Fraction f2)
+        {
+            //create new fraction using reciprocal and multiplication
+            //in the constructor it will be automatically reduced
+            Fraction answer = new Fraction(f1.num * f2.denom, f1.denom * f2.num);
+            return answer;
+        }
+
+
+        public static Fraction operator +(Fraction f1, int num)
+        {
+            return f1 + (Fraction) num;
+        }
+
+        public static Fraction operator -(Fraction f1, int num)
+        {
+            return f1 - (Fraction)num;
+        }
+
+        public static Fraction operator *(Fraction f1, int num)
+        {
+            return f1 * (Fraction)num;
+        }
+
+        public static Fraction operator /(Fraction f1, int num)
+        {
+            return f1 + (Fraction)num;
+        }
+
+        public static Fraction operator +(int num, Fraction f1)
+        {
+            return f1 + (Fraction)num;
+        }
+
+        public static Fraction operator -(int num, Fraction f1)
+        {
+            return (Fraction)num - f1;
+        }
+
+        public static Fraction operator *(int num, Fraction f1)
+        {
+            return f1 * (Fraction)num;
+        }
+
+        public static Fraction operator /(int num, Fraction f1)
+        {
+            return (Fraction)num / f1;
+        }
+
+        public static Double operator +(Fraction f1, double num)
+        {
+            return (double)f1 + num;
+        }
+
+        public static Double operator -(Fraction f1, double num)
+        {
+            return (double)f1 - num;
+        }
+
+        public static Double operator *(Fraction f1, double num)
+        {
+            return (double)f1 * num;
+        }
+
+        public static Double operator /(Fraction f1, double num)
+        {
+            return (double)f1 / num;
+        }
+        public static Double operator +(double num, Fraction f1)
+        {
+            return num + (double)f1;
+        }
+        public static Double operator -(double num, Fraction f1)
+        {
+            return num - (double)f1;
+        }
+        public static Double operator *(double num, Fraction f1)
+        {
+            return num * (double)f1;
+        }
+        public static Double operator /(double num, Fraction f1)
+        {
+            return num / (double)f1;
+        }
+        public static Fraction operator -(Fraction f1)
+        {
+            Fraction negation = new Fraction(-f1.num, f1.denom);
+            //check if both are negative, if yes change to positive 
+            if (negation.num < 0 && negation.denom < 0)
+            {
+                negation = new Fraction(-negation.num, -negation.denom);
+            }
+            return negation;
+        }
+
+        public static bool operator <(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            return f1Num < f2Num;
+        }
+
+        public static bool operator >(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            return f1Num > f2Num;
+        }
+        public static bool operator >=(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            return f1Num >= f2Num;
+        }
+        public static bool operator <=(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            return f1Num <= f2Num;
+        }
+
+        public static bool operator ==(Fraction f1, Fraction f2)
+        {
+            //put with common denom
+            int lcd = Fraction.Lcm(f1, f2);
+            //recalculate the numerators over the lcd
+            int f1Num = f1.num * (lcd / f1.denom);
+            int f2Num = f2.num * (lcd / f2.denom);
+            return f1Num == f2Num;
+        }
+
+        public static bool operator !=(Fraction f1, Fraction f2)
+        {
+            return !(f1 == f2);
+        }
+
+        public static explicit operator double(Fraction f)
+        {
+            return (double)f.Numerator / f.Denominator;
+        }
+
+        public static explicit operator int(Fraction f)
+        {
+            return f.Numerator / f.Denominator;
+        }
+
+        public static explicit operator Fraction(int i)
+        {
+            return new Fraction(i, 1);
+        }
+    }
+}
+
---
+++ UnitTest1.cs
@@ -0,0 +1,373 @@
+//Simcha Mann
+
+using System;
+using ConsoleApp2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ReduceTest()
+        {
+            Fraction f1 = new Fraction(2, 8);
+            Assert.AreEqual(1, f1.Numerator);
+            Assert.AreEqual(4, f1.Denominator);
+        }
+
+        [TestMethod]
+        public void GcfTest()
+        {
+            Fraction f1 = new Fraction(2, 8);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(2, x);
+        }
+
+        [TestMethod]
+        public void GcfTest2()
+        {
+            Fraction f1 = new Fraction(8, 2);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(2, x);
+        }
+
+        [TestMethod]
+        public void GcfTest3()
+        {
+            Fraction f1 = new Fraction(6, 21);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(3, x);
+        }
+
+        [TestMethod]
+        public void GcfTest4()
+        {
+            Fraction f1 = new Fraction(7, 19);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(1, x);
+        }
+
+        [TestMethod]
+        public void GcfTest5()
+        {
+            Fraction f1 = new Fraction(-2, 8);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(2, x);
+        }
+
+        [TestMethod]
+        public void GcfTest6()
+        {
+            Fraction f1 = new Fraction(2, -8);
+            int x = f1.Gcf(f1);
+            Assert.AreEqual(2, x);
+        }
+
+        [TestMethod]
+        public void AdditionTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 9);
+            Fraction f3 = f1 + f2;
+            Assert.AreEqual(new Fraction(5/6), f3 );
+        }
+
+        [TestMethod]
+        public void AdditionTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            int x = 4;
+            Fraction f2 = f1 + x; 
+            Assert.AreEqual(new Fraction(9/2), f2);
+        }
+
+        [TestMethod]
+        public void SubtractTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 9);
+            Fraction f3 = f1 - f2;
+            Assert.AreEqual(new Fraction(1 / 6), f3);
+        }
+
+        [TestMethod]
+        public void SubtractTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            int x = 4;
+            Fraction f2 = f1 - x;
+            Assert.AreEqual(new Fraction(-7 / 2), f2);
+        }
+
+        [TestMethod]
+        public void MultTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 9);
+            Fraction f3 = f1 * f2;
+            Assert.AreEqual(1, f3.Numerator);
+            Assert.AreEqual(6, f3.Denominator);
+        }
+
+        [TestMethod]
+        public void MultTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            int x = 4;
+            Fraction f2 = f1 * x; 
+            Assert.AreEqual(new Fraction(2 / 1), f2);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 9);
+            Fraction f3 = f1 / f2; 
+            Assert.AreEqual(new Fraction(3 / 2), f3);
+        }
+
+        [TestMethod]
+        public void DivideTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            int x = 4;
+            Fraction f2 = f1 / x;
+            Assert.AreEqual(new Fraction(1 / 8), f2);
+        }
+
+        [TestMethod]
+        public void NegTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = -f1;
+            Assert.AreEqual(new Fraction(-1 / 2), f2);
+        }
+
+        [TestMethod]
+        public void NegTest2()
+        {
+            Fraction f1 = new Fraction(-1, 2);
+            Fraction f2 = -f1;
+            Assert.AreEqual(new Fraction(1 / 2), f2);
+        }
+
+        [TestMethod]
+        public void NegTest3()
+        {
+            Fraction f1 = new Fraction(1, -2);
+            Fraction f2 = -f1;
+            Assert.AreEqual(new Fraction(1 / 2), f2);
+        }
+
+        [TestMethod]
+        public void NegTest4()
+        {
+            Fraction f1 = new Fraction(-1, -2);
+            Fraction f2 = -f1;
+            Assert.AreEqual(new Fraction(1 / -2), f2);
+        }
+
+        [TestMethod]
+        public void LCMTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 9);
+            int lcm = Fraction.Lcm(f1, f2);
+            Assert.AreEqual(18, lcm);
+        }
+
+        [TestMethod]
+        public void LCMTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 2);
+            int lcm = Fraction.Lcm(f1, f2);
+            Assert.AreEqual(2, lcm);
+        }
+
+        [TestMethod]
+        public void LCMTest3()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            int lcm = Fraction.Lcm(f1, f2);
+            Assert.AreEqual(4, lcm);
+        }
+
+        [TestMethod]
+        public void GreaterTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsFalse(f1 > f2);
+        }
+
+        [TestMethod]
+        public void GreaterTest1()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsTrue(f2 > f1);
+        }
+
+        [TestMethod]
+        public void GreaterTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+            Assert.IsFalse(f1 > f2);
+        }
+
+        [TestMethod]
+        public void LesserTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsTrue(f1 < f2);
+        }
+
+        [TestMethod]
+        public void LesserTest1()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsFalse(f2 < f1);
+        }
+
+        [TestMethod]
+        public void LesserTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+            Assert.IsFalse(f1 > f2);
+        }
+
+        [TestMethod]
+        public void GreaterEqualTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsFalse(f1 >= f2);
+        }
+
+        [TestMethod]
+        public void GreaterEqualTest1()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsTrue(f2 >= f1);
+        }
+
+        [TestMethod]
+        public void GreaterEqualTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+            Assert.IsTrue(f1 >= f2);
+        }
+
+        [TestMethod]
+        public void LesserEqualTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsTrue(f1 <= f2);
+        }
+
+        [TestMethod]
+        public void LesserEqualTest1()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsFalse(f2 <= f1);
+        }
+
+        [TestMethod]
+        public void LesserEqualTest2()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+            Assert.IsTrue(f1 >= f2);
+        }
+
+        [TestMethod]
+        public void EqualTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(2, 4);
+            Assert.IsTrue(f1 == f2);
+        }
+
+        [TestMethod]
+        public void EqualTest1()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(3, 4);
+            Assert.IsFalse(f2 == f1);
+        }
+
+        [TestMethod]
+        public void NotEqualTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Fraction f2 = new Fraction(1, 2);
+            Assert.IsFalse(f1 != f2);
+        }
+
+        [TestMethod]
+        public void NotEqualTest1()
+        {
+            Fraction f1 = new Fraction(1, 4);
+            Fraction f2 = new Fraction(6, 24);
+            Assert.IsFalse(f1 != f2);
+        }
+
+        [TestMethod]
+        public void NotEqualTest2()
+        {
+            Fraction f1 = new Fraction(1, 7);
+            Fraction f2 = new Fraction(6, 24);
+            Assert.IsTrue(f1 != f2);
+        }
+
+        [TestMethod]
+        public void CastDoubleTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Assert.AreEqual(0.5 ,(double)f1);
+        }
+
+        [TestMethod]
+        public void CastIntTest()
+        {
+            Fraction f1 = new Fraction(1, 2);
+            Assert.AreEqual(0, (int)f1);
+        }
+
+        [TestMethod]
+        public void CastIntTest1()
+        {
+            Fraction f1 = new Fraction(8, 2);
+            Assert.AreEqual(4, (int)f1);
+        }
+
+        [TestMethod]
+        public void CastFracTest()
+        {
+            int x = 4;
+            Fraction f1 = new Fraction(4, 1);
+            Assert.AreEqual(f1, (Fraction)x);
+        }
+
+        [TestMethod]
+        public void CastFracTest1()
+        {
+            int x = 1;
+            Fraction f1 = new Fraction(1, 1);
+            Assert.AreEqual(f1, (Fraction)x);
+        }
+    }
+}
